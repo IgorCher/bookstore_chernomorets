1f2d3797229631b1093c69308dc38490f4b4929f
IgorChernov <ichernomorr@gmail.com>
2023-07-30 15:08:30 +0300

    Update UserController, add BookController

--- src/main/java/com/belhard/bookstore/web/BookController.java
+++ src/main/java/com/belhard/bookstore/web/BookController.java
@@ -2,12 +2,12 @@ package com.belhard.bookstore.web;
 
 import com.belhard.bookstore.data.connection.ConnectionManager;
 import com.belhard.bookstore.data.connection.impl.ConnectionManagerImpl;
-import com.belhard.bookstore.data.dao.impl.UserDaoImpl;
+import com.belhard.bookstore.data.dao.impl.BookDaoImpl;
+import com.belhard.bookstore.data.dto.BookDto;
 import com.belhard.bookstore.platform.ConfigurationManager;
 import com.belhard.bookstore.platform.impl.ConfigurationManagerImpl;
-import com.belhard.bookstore.service.UserService;
-import com.belhard.bookstore.service.impl.UserServiceImpl;
-import com.belhard.bookstore.data.dto.UserDto;
+import com.belhard.bookstore.service.BookService;
+import com.belhard.bookstore.service.impl.BookServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,36 +17,36 @@ import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
-@WebServlet("/user")
+@WebServlet("/book")
 @Log4j2
-public class UserController extends HttpServlet {
-    private final UserService userService;
+public class BookController extends HttpServlet {
+    private final BookService bookService;
     private static final ConfigurationManager configurationManager = new ConfigurationManagerImpl();
     private static final String URL = configurationManager.getProperty("db.url");
     private static final String LOGIN = configurationManager.getProperty("db.login");
     private static final String PASSWORD = configurationManager.getProperty("db.password");
     private static final String DRIVER = configurationManager.getProperty("db.driver");
 
-    public UserController(){
+    public BookController(){
         ConnectionManager connectionManager = new ConnectionManagerImpl(URL, LOGIN, PASSWORD, DRIVER);
-        userService = new UserServiceImpl(new UserDaoImpl(connectionManager));
+        bookService = new BookServiceImpl(new BookDaoImpl(connectionManager));
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String idRaw = req.getParameter("id");
         if(idRaw == null){
-            resp.getWriter().println("<html><h1>Bad request!</h1><p>Need to set parameters</p></html>");
+            resp.getWriter().println("<html><h1>Bad request!!!</h1><p>Need to set parameters</p></html>");
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
         try {
             Long id = Long.valueOf(idRaw);
-            UserDto user = userService.getById(id);
+            BookDto bookDto = bookService.getById(id);
 
-            String html = "<html><h1>User</h1><h2>ID: " + user.getId() + "</h2>" +
-                    "<h2>Name: " + user.getName() + " " + user.getLastName() + "</h2>" +
-                    "<h3></h3><p>Email: " + user.getEmail() + "</p></html>";
+            String html = "<html><h1>Book</h1><h2>ID: " + bookDto.getId() + "</h2>" +
+                    "<h2>Title: " + bookDto.getTitle() + "</h2>" +
+                    "<h3>Author: " + bookDto.getAuthor() + "</h3><p>Price: " + bookDto.getPrice() + "</p></html>";
             resp.getWriter().println(html);
         }catch (RuntimeException e) {
             log.error("Bad request with parameter: id = " + idRaw);
